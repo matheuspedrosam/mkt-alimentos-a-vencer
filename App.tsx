a0fd608a24eef6d0ee0b887eb155c0f52deadfc7
matheuspedrosam <matheuspedrosa2002@gmail.com>
2024-12-10 14:32:54 -0300

    eas update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import * as Updates from 'expo-updates';
 
 export interface AppProps {
 }
 
 export function App (props: AppProps) {
-    return (
-      <View>
-         <Text>App</Text>
-      </View>
-    );
+  async function onFetchUpdateAsync() {
+    try{
+      const update = await Updates.checkForUpdateAsync();
+
+      if(update.isAvailable){
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error){
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  })
+
+  return (
+    <View>
+        <Text>App</Text>
+    </View>
+  );
 }
